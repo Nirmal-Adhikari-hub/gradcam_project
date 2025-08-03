9f28227922bd797cab74f4596a548e5a3d94bdbf
Nirmal-Adhikari-hub <adhikareen@gmail.com>
2025-08-03 22:42:12 +0900

    3d,3d activations handling reshape_transform

--- per_layer.sh
+++ per_layer.sh
@@ -2,5 +2,5 @@ python -m gradcam.gradcam_perlayer_slowfast \
     --config ./slowfast/configs/baseline.yaml \
     --mode test \
     --index 0 \
-    --slowfast_ckpt /path/to/your.ckpt \
+    --slowfast_ckpt /shared/home/xvoice/nirmal/gradcam/checkpoints/slow_fast_phoenix2014_dev_18.01_test_18.28.pt \
     --verbose
