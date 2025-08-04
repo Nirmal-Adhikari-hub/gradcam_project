55711518a448402df47efe754594093a33ecaf68
Nirmal-Adhikari-hub <adhikareen@gmail.com>
2025-08-04 14:18:56 +0900

    trying for train set coz got <unk> for the last test set

--- per_layer.sh
+++ per_layer.sh
@@ -1,6 +1,6 @@
 python -m gradcam.gradcam_perlayer_slowfast \
     --config ./slowfast/configs/baseline.yaml \
-    --mode test \
-    --index 0 \
+    --mode train \
+    --index 68 \
     --slowfast_ckpt /shared/home/xvoice/nirmal/gradcam/checkpoints/slow_fast_phoenix2014_dev_18.01_test_18.28.pt \
     --verbose
